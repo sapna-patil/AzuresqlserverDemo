dcee9fe7dff5f17c8c4dda2845d9db943269c033
Sapna Patil <sapna.patil@yash.com>
2020-05-28 23:38:02 +0530

    WebjobDemo code

---
+++ WebJobDemo/WebJobDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebJobDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi sapna, webjob is running fine....");
+            Console.ReadLine();
+        }
+    }
+}
